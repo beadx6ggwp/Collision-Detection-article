7f890c344aef8a2e63b1a552e52c79894d2ca609
DavidHsu <davidmd9830415@gmail.com>
2017-08-28 03:06:20 +0800

    update

--- test.js
+++ test.js
@@ -46,6 +46,7 @@ function Box(x, y, w, h) {
     this.pos = new Vector(x, y);// center
     this.w = w;
     this.h = h;
+    // 以順時針紀錄矩形的四個角
     this.corners = [
         new Vector(w / 2, -h / 2),
         new Vector(w / 2, h / 2),
@@ -57,15 +58,17 @@ function Box(x, y, w, h) {
     this.getVertices = function () {
         var vertices = [];
 
-        // Clockwise
+        // 順時針走訪角落
         for (var i = 0; i < this.corners.length; i++) {
             var p1 = this.corners[i];
 
             var vec = new Vector(this.pos.x + p1.x, this.pos.y + p1.y);
+            // 將各個角以物體中心為參考點來旋轉
             vec.rotateRefPoint(this.directionAngle, this.pos);
 
             vertices.push(vec);
         }
+        // 最後回傳以物體中心為參考點選轉後的角落
         return vertices;
     }
 
@@ -88,7 +91,7 @@ function Box(x, y, w, h) {
         p2 = vertices[0];
         n = new Vector(p2.x - p1.x, p2.y - p1.y).normalL();
         norms.push(n);
-
+        // 最後傳回這個物體所有邊上的左法向量
         return norms;
     }
 }
@@ -227,10 +230,120 @@ function update(dt) {
             if (i == j) continue;
 
             var isCollided = !SAT_Collision(shapes[i], shapes[j]);
-            if(isCollided){
+            if (isCollided) {
                 // ...碰撞事件...
-            }            
+            }
         }
     }
-    
+
+}
+
+
+
+function rotate(vec, angle) {
+    var new_x = (vec.x * Math.cos(angle)) - (vec.y * Math.sin(angle));
+    var new_y = (vec.x * Math.sin(angle)) + (vec.y * Math.cos(angle));
+
+    vec.x = new_x;
+    vec.y = new_y;
+}
+
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+// 取得這個自己的長度
+Vector.prototype.length = function () {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+}
+// 取得自己與vec2的內積
+Vector.prototype.dot = function (vec2) {
+    return this.x * vec2.x + this.y * vec2.y;
+}
+// 取得自己在vec2上的正射影長
+Vector.prototype.projectLengthOnto = function (vec2) {
+    var dotProduct = this.dot(vec2);
+    var len = vec2.length();
+    return dotProduct / len;
+}
+// 取得自己的左法向量
+Vector.prototype.normalL = function () {
+    return new Vector(-this.y, this.x);
+}
+// 取得自己的右法向量
+Vector.prototype.normalR = function () {
+    return new Vector(this.y, -this.x);
+}
+// angle:弧度, refP:參考點
+// 作用: 以refP為參考點，旋轉angle角
+//使用方式
+
+var vec1 = new Vector(3, 4);
+var vec2 = new Vector(1, 0);
+vec1.length() // => 5
+vec1.dot(vec2) // => 3
+vec1.projectLengthOnto(vec2) // =>3
+vec1.normalL() // = (-4, 3)
+vec1.normalR() // = (4, -3)
+
+Vector.prototype.rotate = function (angle) {
+    let new_x = (this.x * Math.cos(angle)) - (this.y * Math.sin(angle));
+    let new_y = (this.x * Math.sin(angle)) + (this.y * Math.cos(angle));
+
+    this.x = new_x;
+    this.y = new_y;
+};
+Vector.prototype.rotateRefPoint = function (angle, refP) {
+    let new_x = (this.x - refP.x) * Math.cos(angle) - (this.y - refP.y) * Math.sin(angle) + refP.x;
+    let new_y = (this.y - refP.y) * Math.cos(angle) + (this.x - refP.x) * Math.sin(angle) + refP.y;
+    this.x = new_x;
+    this.y = new_y;
+};
+
+// 取得vec1與vec2的內積
+function dot(vec1, vec2) {
+    return vec1.x * vec2.x + vec1.y * vec2.y;
+}
+// 取得vec1在vec2上的正射影長
+function projectLengthOnto(vec1, vec2) {
+    var dotProduct = dot(vec1, vec2);
+    var len = Math.sqrt(vec2.x * vec2.x + vec2.y * vec2.y);
+    return dotProduct / len;
+}
+// 取得vec的左法向量
+function getNormalL(vec) {
+    return new Vector(-vec.y, vec.x);
+}
+// 取得vec的右法向量
+function getNormalR(vec) {
+    return new Vector(vec.y, -vec.x);
+}
+
+// getMinMax(頂點陣列,分離軸)
+function getMinMax(vertices, axis) {
+    // 先以第一個角落投影為標準
+    var min_DotProduct = vertices[0].projectLengthOnto(axis),
+        max_DotProduct = vertices[0].projectLengthOnto(axis);
+
+    for (var i = 1; i < vertices.length; i++) {
+        // 取得當前要比對的投影長度
+        var temp = vertices[i].projectLengthOnto(axis);
+        // 如果比當前最小的更小，紀錄它
+        if (temp < min_DotProduct) {
+            min_DotProduct = temp;
+            min_index = i;
+        }
+        // 如果比當前最小的更大，紀錄它
+        if (temp > max_DotProduct) {
+            max_DotProduct = temp;
+            max_index = i;
+        }
+    }
+
+    var result = {
+        min: min_DotProduct,
+        max: max_DotProduct
+    };
+    // 最後傳回一個物件包含min、max屬性
+    return result;
 }
